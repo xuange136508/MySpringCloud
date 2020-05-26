50ba1e552c5a9fec9fe62af187dde50549f19168
xiex <xiex@bairuitech.com>
2020-05-26 18:09:58 +0800

    测试

--- parent/springcloud_consumer/src/main/java/com/springcloud/consumer/ConsumerApplication.java
+++ parent/springcloud_consumer/src/main/java/com/springcloud/consumer/ConsumerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class ConsumerApplication
 {
+    //初始化
     public static void main(String[] args)
     {
         SpringApplication.run(ConsumerApplication.class, args);
